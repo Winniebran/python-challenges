c38c5e4eacdcd01e60fb9bcf94da32979ef65e0b
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-02-23 10:37:31 -0300

    feat: add fifth challenge

---
+++ challenge_5/readme.md
@@ -0,0 +1,7 @@
+# Challenge 5 - Band Kamp
+
+Projeto em Django e Django Rest Framework "legado" que inicialmente já está desenvolvido com APIView e sqlite3, fazendo a transição para Generic Views, Model Serializer e Postgres, além de documentar e de verificar/manter a integridade das funcionalidades já existentes.
+<br><br>
+
+[Documentação](http://127.0.0.1:8000/api/docs/redoc/)<br>
+Obs: O servidor precisa estar rodando para que sejá possível visualizar a documentação.
---
+++ challenge_5/users/views.py
@@ -0,0 +1,17 @@
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
+from .permissions import IsAccountOwner
+
+
+class UserView(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
